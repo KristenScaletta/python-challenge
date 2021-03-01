1a012f224709ac4e6114f055a4ef087804ee61b6
KristenScaletta <78096475+KristenScaletta@users.noreply.github.com>
2021-03-01 17:06:47 -0600

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -17,11 +17,11 @@ with open(csvpath) as csvfile:
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
 
-    print(csvreader)
+    #print(csvreader)
 
     # Read the header row first 
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
+    #print(f"CSV Header: {csv_header}")
 
    
     total = 0
@@ -30,8 +30,9 @@ with open(csvpath) as csvfile:
     list = []
       
     for row in csvreader:
-        # Count how many months by counting length of each row to convert to int. The limitation of this approach is the string must contain the same amount of characters, as it does in this data set. I'd love advice on how to count rows that contain a string in a more streamlined manner that is more broadly applicable.
-        months += len(row[0])
+        # Count how many months. 
+        if len(row[0]) != 0:
+            months = months + 1
         
         # Read each row of data after the header and total the second column. Source: https://www.reddit.com/r/learnpython/comments/5djs0i/summing_columns_in_csv_file/
         total += float(row[1])
@@ -39,19 +40,18 @@ with open(csvpath) as csvfile:
         currentcount = row[1]
         list.append(currentcount)
         currentcount=0
-        max = max(list)
-        if int(row[1]) == int(max):
-            maxmonth = row[0]
+        #max = max(list)
+        #if int(row[1]) == int(max):
+         #   maxmonth = row[0]
    #print(list)
-    #calculates the total months by dividing the months by the number of characters in the string (which in this case works since each string in the first column has 8 characters.)
-    total_months = int(((months) / 8))
+    
 
     #outputs the P&L total
     print(f"Financial Analysis")
     print(f"------------------")
-    print(f"Total Months: {total_months}")
+    print(f"Total Months: {months}")
     print(f"Total: ${total}")
     print(f"Average Change: ")
-    print(f"Greatest Increase in Profits: {maxmonth} (${max(list)})")
-    print(f"Greatest Decrease in Profits: ${min(list)}")  
+    print(f"Greatest Increase in Profits: (${max(list)})")
+    #print(f"Greatest Decrease in Profits: {maxmonth} ${min(list)}")  
        
