8daba6cb064e57032be62090f17052425007b814
KristenScaletta <78096475+KristenScaletta@users.noreply.github.com>
2021-03-01 08:23:10 -0600

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -26,18 +26,23 @@ with open(csvpath) as csvfile:
    
     total = 0
     months = 0
-   
-        
+    #i = 0
+    list = []
+      
     for row in csvreader:
         # Count how many months by counting length of each row to convert to int. The limitation of this approach is the string must contain the same amount of characters, as it does in this data set. I'd love advice on how to count rows that contain a string in a more streamlined manner that is more broadly applicable.
         months += len(row[0])
         
         # Read each row of data after the header and total the second column. Source: https://www.reddit.com/r/learnpython/comments/5djs0i/summing_columns_in_csv_file/
         total += float(row[1])
-    #need to work on this. Doesn't work.
-        list = [row[1]]
-    
-    
+    #need to work on this. Doesn't pull min and max.
+        currentcount = row[1]
+        list.append(currentcount)
+        currentcount=0
+        max = max(list)
+        if int(row[1]) == int(max):
+            maxmonth = row[0]
+   #print(list)
     #calculates the total months by dividing the months by the number of characters in the string (which in this case works since each string in the first column has 8 characters.)
     total_months = int(((months) / 8))
 
@@ -47,6 +52,6 @@ with open(csvpath) as csvfile:
     print(f"Total Months: {total_months}")
     print(f"Total: ${total}")
     print(f"Average Change: ")
-    print(f"Greatest Increase in Profits: ${max(list)}")
-    print(f"Greatest Decrease in Profits: ")  
+    print(f"Greatest Increase in Profits: {maxmonth} (${max(list)})")
+    print(f"Greatest Decrease in Profits: ${min(list)}")  
        
