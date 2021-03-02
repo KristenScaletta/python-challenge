1a5d6733e3b77a544837ff4163d8665c4dac8b3f
KristenScaletta <78096475+KristenScaletta@users.noreply.github.com>
2021-03-02 09:52:37 -0600

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -41,23 +41,40 @@ with open(csvpath) as csvfile:
         total += float(row[1])
         # Calculates mininmum decrease and maximim increase.
         
-        if int(row[1]) > int(maxincrease):
-            maxincrease = int(row[1])
+        if float(row[1]) > float(maxincrease):
+            maxincrease = float(row[1])
             monthincrease = str(row[0])
-        if int(row[1]) < int(maxdecrease):
-            maxdecrease = int(row[1])
+        if float(row[1]) < float(maxdecrease):
+            maxdecrease = float(row[1])
             monthdecrease = str(row[0])
 
-        
-        
-    
-
- #outputs the P&L total
+ #calculates average change--need formula
+    change = (maxdecrease - maxincrease)/total
+ #outputs the P&L total. Neet to output to a text file.
     print(f"Financial Analysis")
     print(f"------------------")
     print(f"Total Months: {months}")
     print(f"Total: ${total}")
-    print(f"Average Change: ")
+    print(f"Average Change: {change}") #Need formula
     print(f"Greatest Increase in Profits: {monthincrease} (${maxincrease})")
     print(f"Greatest Decrease in Profits: {monthdecrease} ({maxdecrease})")  
+       
+# Dependencies
+import os
+import csv
+
+# Specify the file to write to
+output_path = os.path.join("..", "PyBank/Analysis/pybankanalysis.txt")
+
+# Open the file using "write" mode. Specify the variable to hold the contents
+with open(output_path, 'w') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    
+    csvwriter.writerow(f"Financial Analysis")
+    csvwriter.writerow(f"------------------")
+    csvwriter.writerow(f"Total Months: {months}")
+    csvwriter.writerow(f"Total: ${total}")
+    csvwriter.writerow(f"Average Change: {change}") #Need formula
+    csvwriter.writerow(f"Greatest Increase in Profits: {monthincrease} (${maxincrease})")
+    csvwriter.writerow(f"Greatest Decrease in Profits: {monthdecrease} ({maxdecrease})")  
        
