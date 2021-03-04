ee62d612c9242a4ab0f5f678048ddef6af4bbef7
KristenScaletta <78096475+KristenScaletta@users.noreply.github.com>
2021-03-04 07:49:44 -0600

    Update main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,9 +7,6 @@ import csv
 csvpath = os.path.join('..', 'PyPoll/Resources/02-Homework_03-Python_Instructions_PyPoll_Resources_election_data.csv')
 
 
-
-
-
 #Use CSV module to read CSV
 
 with open(csvpath) as csvfile:
@@ -25,10 +22,12 @@ with open(csvpath) as csvfile:
     total_votes = 0
     candidate_votes = [0, 0, 0, 0, 0, 0]
     percent_votes = [0, 0, 0, 0, 0, 0]
-    candidate_number = 0
+    #candidate_number = 0
     votecount = 0
     percent = 0
-            
+    #count = 0
+         
+    
     for row in csvreader:
         
 
@@ -45,26 +44,30 @@ with open(csvpath) as csvfile:
             
             candidate_list.append(candidate_name)
 
-        #insert vote count in right spot tied to candidate number. Doesn't yet work
-        candidate_number = len(candidate_list)
+
+
+#insert vote count in right spot tied to candidate number. 
+
     
+list_length = int(len(candidate_list))
+w = 0
+while list_length > 0: 
+    with open(csvpath) as csvfile:
 
-        candidate_number = int(candidate_number) - 1
-        votecount = int(candidate_votes[candidate_number]) + 1
-        candidate_votes[candidate_number] = votecount 
-        votecount = 0
-        candidate_number = 0
+    # CSV reader specifies delimiter and variable that holds contents
+        csvreader = csv.reader(csvfile, delimiter=',') 
 
-        list_length = int(len(candidate_list)) 
+    # Read the header row first 
+        csv_header = next(csvreader) 
         
-    
-       
-
+        for row in csvreader:
+            if candidate_list[w] == row[2]:
+                votecount = votecount + 1
+                candidate_votes[w] = votecount
+        w = w + 1
+    votecount = 0
+    list_length = list_length - 1
         
-        
-       
-        
-   
     #Establishes dictionary of candidate name and votes           
     
    
@@ -94,6 +97,7 @@ with open(csvpath) as csvfile:
         print(f"{candidate_info['Name'][i]}: {float(candidate_info['Percent'][i])}% ({candidate_info['Votes'][i]})")
 
        #determine winner
+        winner_name = ""
         if candidate_info["Votes"][i] > winner:
             winner = candidate_info["Votes"][i]
             winner_name = candidate_info["Name"][i]
