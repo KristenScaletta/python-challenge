2a9790041d888e69bb9474277ed278fc6218964b
KristenScaletta <78096475+KristenScaletta@users.noreply.github.com>
2021-03-04 07:45:01 -0600

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,15 +1,10 @@
 # Import the os module
 import os
 
-# Module for reading CSV files
+# Module for reading CSV files (Note..I could not goet 'Pybank', 'Resources', 'filename' to work on my machine. Advice would be appreciated.)
 import csv
 
 csvpath = os.path.join('..', 'PyBank/Resources/02-Homework_03-Python_Instructions_PyBank_Resources_budget_data.csv')
-
-#Dictionary to store data
-
-       
-
 
 #Use CSV module to read CSV
 
@@ -18,27 +13,26 @@ with open(csvpath) as csvfile:
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
 
-    #print(csvreader)
-
     # Read the header row first 
     csv_header = next(csvreader)
-    #print(f"CSV Header: {csv_header}")
 
-   
+    #Declare variables
     total = 0
     months = 0
     maxincrease = 0 
     maxdecrease = 0  
     monthincrease = "a"
     monthdecrease = "b"
+    
+    #Reads each row in the open CSV file
     for row in csvreader:
-        #print_info(row)
-        #Count how many months. 
+        #Count total months if the row is not null. 
         if len(row[0]) != 0:
             months = months + 1
         
-        # Read each row of data after the header and total the second column. Source: https://www.reddit.com/r/learnpython/comments/5djs0i/summing_columns_in_csv_file/
+        #Calculates total. Reads each row of data and totals the second column. Source: https://www.reddit.com/r/learnpython/comments/5djs0i/summing_columns_in_csv_file/
         total += float(row[1])
+        
         # Calculates mininmum decrease and maximim increase.
         
         if float(row[1]) > float(maxincrease):
@@ -48,33 +42,33 @@ with open(csvpath) as csvfile:
             maxdecrease = float(row[1])
             monthdecrease = str(row[0])
 
- #calculates average change--need formula
-    change = (maxdecrease - maxincrease)/total
- #outputs the P&L total. Neet to output to a text file.
+ #Using variables cacculated, calculates the average change--need formula
+    change = float(total)/float(months)
+ 
+ #outputs the requested data in a terminal.
     print(f"Financial Analysis")
     print(f"------------------")
     print(f"Total Months: {months}")
     print(f"Total: ${total}")
-    print(f"Average Change: {change}") #Need formula
+    print(f"Average Change: {float(change)}") #Need formula
     print(f"Greatest Increase in Profits: {monthincrease} (${maxincrease})")
     print(f"Greatest Decrease in Profits: {monthdecrease} ({maxdecrease})")  
        
+#Outputs the requested data in a new text file. Source: https://www.geeksforgeeks.org/reading-writing-text-files-python/
 # Dependencies
 import os
-import csv
 
 # Specify the file to write to
 output_path = os.path.join("..", "PyBank/Analysis/pybankanalysis.txt")
 
 # Open the file using "write" mode. Specify the variable to hold the contents
-with open(output_path, 'w') as csvfile:
-    csvwriter = csv.writer(csvfile)
-    
-    csvwriter.writerow(f"Financial Analysis")
-    csvwriter.writerow(f"------------------")
-    csvwriter.writerow(f"Total Months: {months}")
-    csvwriter.writerow(f"Total: ${total}")
-    csvwriter.writerow(f"Average Change: {change}") #Need formula
-    csvwriter.writerow(f"Greatest Increase in Profits: {monthincrease} (${maxincrease})")
-    csvwriter.writerow(f"Greatest Decrease in Profits: {monthdecrease} ({maxdecrease})")  
-       
+with open(output_path, 'w') as txtfile:
+
+#Adds output to the new text file or overwrites file with the below outputs.
+    txtfile.writelines(f"Financial Analysis\n")
+    txtfile.writelines(f"------------------\n")
+    txtfile.writelines(f"Total Months: {months}\n")
+    txtfile.writelines(f"Total: ${total}\n")
+    txtfile.writelines(f"Average Change: {change}\n") #Need formula
+    txtfile.writelines(f"Greatest Increase in Profits: {monthincrease} (${maxincrease})\n")
+    txtfile.writelines(f"Greatest Decrease in Profits: {monthdecrease} ({maxdecrease})\n")  
