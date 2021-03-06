bc212e86ece53b416f3923ca20654afc7d17a408
KristenScaletta <78096475+KristenScaletta@users.noreply.github.com>
2021-03-06 14:41:59 -0600

    Update main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -6,7 +6,12 @@ import csv
 
 csvpath = os.path.join('..', 'PyPoll/Resources/02-Homework_03-Python_Instructions_PyPoll_Resources_election_data.csv')
 
+#Variable and disctionaries to store data
+total_votes = 0
+candidate_dict={}
+candidate_percent_dict = {}
 
+print(dict)
 #Use CSV module to read CSV
 
 with open(csvpath) as csvfile:
@@ -16,98 +21,60 @@ with open(csvpath) as csvfile:
 
     # Read the header row first 
     csv_header = next(csvreader)
-
-    #Lists to store data
-    candidate_list = []
-    total_votes = 0
-    candidate_votes = [0, 0, 0, 0, 0, 0]
-    percent_votes = [0, 0, 0, 0, 0, 0]
-    #candidate_number = 0
-    votecount = 0
-    percent = 0
-    #count = 0
-         
     
     for row in csvreader:
-        
-
-        #counts total number of votes cast
-        if len(row[0]) != 0:
-            total_votes = total_votes + 1
-        
-        #makes list of all unique candidates.
-        candidate_name = row[2]
-
-        
-
-        if candidate_name not in candidate_list:
-            
-            candidate_list.append(candidate_name)
-
-
-
-#insert vote count in right spot tied to candidate number. 
-
+        #calculates total votes
+        total_votes = total_votes + 1
+        #calculates votes per candidate. Source: Mohammad's assistance.
     
-list_length = int(len(candidate_list))
-w = 0
-while list_length > 0: 
-    with open(csvpath) as csvfile:
-
-    # CSV reader specifies delimiter and variable that holds contents
-        csvreader = csv.reader(csvfile, delimiter=',') 
-
-    # Read the header row first 
-        csv_header = next(csvreader) 
+        candidate = row[2]
+        if candidate in candidate_dict:
+            candidate_dict[candidate] += 1
+        else:
+            candidate_dict[candidate] = 1
         
-        for row in csvreader:
-            if candidate_list[w] == row[2]:
-                votecount = votecount + 1
-                candidate_votes[w] = votecount
-        w = w + 1
-    votecount = 0
-    list_length = list_length - 1
-        
-    #Establishes dictionary of candidate name and votes           
-    
-   
-    candidate_info = {
-             "Name": candidate_list,
-            "Votes": candidate_votes,
-            "Percent": percent_votes
-            }
+        #calculates percentage of votes per candidate
+        candidate_percent_dict[candidate] = f"{round(candidate_dict[candidate] / total_votes * 100, 2)}"
 
-    
-    #Prints Election Results Header and Total Votes
-    print(f"Election Results")
-    print(f"------------------")
-    print(f"Total Votes: {total_votes}")
-    print(f"------------------")
-    
-    #Prints candidates and individual vote totals
-    
-    i = 0
-    list_length = int(len(candidate_list)) 
-    winner = 0
-    while list_length > 0:
-       
-        
-        candidate_info["Percent"][i] = candidate_info["Votes"][i]/total_votes * 100
-        
-        print(f"{candidate_info['Name'][i]}: {float(candidate_info['Percent'][i])}% ({candidate_info['Votes'][i]})")
-
-       #determine winner
-        winner_name = ""
-        if candidate_info["Votes"][i] > winner:
-            winner = candidate_info["Votes"][i]
-            winner_name = candidate_info["Name"][i]
-        i = i + 1
-        list_length = list_length - 1
-    
-    print(f"------------------")
-
-    #Prints winner
-    print(f"Winner: {winner_name}") 
-    print(f"------------------")
+#prints results
+print(f"Election Results")
+print(f"------------------")
+print(f"Total Votes: {total_votes}")
+print(f"------------------")
+winner_votes = 0
+#finds results for each candidate in the dictionaries
+for candidate in candidate_dict and candidate_percent_dict:
+    print(f"{candidate}: {candidate_dict[candidate]}, {candidate_percent_dict[candidate]}%") 
+    #calculates winner
+    if candidate_dict[candidate] > winner_votes:
+        winner_votes = candidate_dict[candidate]
+        winner = candidate
+#prints winner and spaces
+print(f"------------------")
+print(f"The winner is: {winner}")
+print(f"------------------")
   
-       
+#Outputs the requested data in a new text file. Source: https://www.geeksforgeeks.org/reading-writing-text-files-python/
+# Dependencies
+import os
+
+# Specify the file to write to
+output_path = os.path.join("..", "PyPoll/Analysis/pypollanalysis.txt")
+
+# Open the file using "write" mode. Specify the variable to hold the contents
+with open(output_path, 'w') as txtfile:
+
+#Adds output to the new text file or overwrites file with the below outputs. Same as print above.
+    txtfile.writelines(f"Election Results\n")
+    txtfile.writelines(f"------------------\n")
+    txtfile.writelines(f"Total Votes: {total_votes}\n")
+    txtfile.writelines(f"------------------\n")
+    winner_votes = 0
+    for candidate in candidate_dict and candidate_percent_dict:
+        txtfile.writelines(f"{candidate}: {candidate_dict[candidate]}, {candidate_percent_dict[candidate]}%\n") 
+        if candidate_dict[candidate] > winner_votes:
+            winner_votes = candidate_dict[candidate]
+            winner = candidate
+    txtfile.writelines(f"------------------\n")
+    txtfile.writelines(f"The winner is: {winner}\n")
+    txtfile.writelines(f"------------------\n")
